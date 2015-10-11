b85702a289fd9bcaf447f91ae068d486fae446a5
Ken Iseli <ken.iseli@gmail.com>
2015-10-11 22:57:23 +0200

    file utils now handles _all_ file associated stuff

--- src/mada_1/App.java
+++ src/mada_1/App.java
@@ -1,13 +1,11 @@
 package mada_1;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.OpenOption;
-import java.util.ArrayList;
 import java.util.List;
 
 public class App {
+	private static final String CHIFFRE_TEXT_FILE_NAME = "/chiffre.txt";
+	private FileUtils fileUtils = new FileUtils();
 
 	public static void main(String[] args) {
 		App app = new App();
@@ -16,25 +14,23 @@ public class App {
 
 	private void doTheRsa() {
 		// TODO generate tuples
+		
 
 		// encrypt text.txt in chiffre.txt
-		Tuple tupleFromFile = getTupleFromFile("/pk.txt");
-		String plainText = readContentFromFile("/text.txt");
-		File tempFile = encrypt(tupleFromFile, plainText);
+		Tuple publicKey = getTupleFromFile("/pk.txt");
+		File textFile = fileUtils.getFileFromClassPath("/text.txt");
+		String plainText = fileUtils.readContentFromFile(textFile);
+		String encrypted = encrypt(publicKey, plainText);
 
-		// String chiffreFile = tempFile.getName();
-		
-		
-		// TODO decrypt chiffre.txt
-		File chiffreFile = new File("/chiffre.txt");
-		Tuple tupleFromPrivateKey = getTupleFromFile("/sk.txt");
-		String chiffreText = readContentFromFile(tempFile.getAbsolutePath());
-		boolean done = decrypt(Tuple.parseTuple("(209,173)"), chiffreText);
-		
+		File chiffreFile = fileUtils.getFileFromClassPath(CHIFFRE_TEXT_FILE_NAME);
+		Tuple privateKey = getTupleFromFile("/sk.txt");
+		String chiffreText = fileUtils.readContentFromFile(chiffreFile);
+		String decrypted = decrypt(privateKey, chiffreText);
+		System.out.println(plainText + " - " + encrypted + " - " + decrypted);
 	}
 
 	private Tuple getTupleFromFile(String keyFileName) {
-		List<String> lines = readLinesFromFile(keyFileName);
+		List<String> lines = fileUtils.readLinesFromFile(keyFileName);
 		int numberOfLines = lines.size();
 		if (numberOfLines != 1) {
 			String message = String.format("Expected 1 line (with 1 key) from file [%s] but got [%s].", keyFileName,
@@ -45,48 +41,18 @@ public class App {
 		return key;
 	}
 
-	private String readContentFromFile(String fileName) {
-		List<String> readLinesFromFile = readLinesFromFile(fileName);
-		String content = "";
-		for (String line : readLinesFromFile) {
-			content = String.format("%s%s", content, line);
-		}
-		return content;
-	}
-	
-	private List<String> readLinesFromFile(String fileName) {
-		File file =  new File(fileName);
-		if (!file.exists()) {
-			file = new File(getClass().getResource(fileName).getFile());
-		}
-		List<String> lines = FileUtils.readFromFile(file);
-		return lines;
-	}
-
-	private File encrypt(Tuple tupleFromFile, String plainText) {
+	private String encrypt(Tuple tuple, String plainText) {
 		Encryptor encryptor = new Encryptor();
-		String chiffreText = encryptor.encrypt(tupleFromFile, plainText);
+		String chiffreText = encryptor.encrypt(tuple, plainText);
 
-		File tempFile = null;
-		try {
-			//tempFile = File.createTempFile("chiffre", ".txt");
-			tempFile = new File("chiffre.txt");
-			ArrayList<String> lines = new ArrayList<String>();
-			lines.add(chiffreText);
-			OpenOption[] options = new OpenOption[0];
-			Files.write(tempFile.toPath(), lines, options);
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-		return tempFile;
+		File file = fileUtils.getFileFromClassPath(CHIFFRE_TEXT_FILE_NAME);
+		fileUtils.writeToFile(file, chiffreText);
+		return chiffreText;
 	}
-	
-	private boolean decrypt(Tuple tuplePrivateKey, String chiffreText) {
+
+	private String decrypt(Tuple privateKey, String chiffreText) {
 		Decryptor decryptor = new Decryptor();
-		String plainText = decryptor.decrypt(tuplePrivateKey, chiffreText);
-		System.out.println(plainText);
-		
-		return true;
+		String plainText = decryptor.decrypt(privateKey, chiffreText);
+		return plainText;
 	}
 }
