07258a058a1803c27a1162fd3a159805473b01a5
Ken Iseli <ken.iseli@gmail.com>
2015-10-11 23:15:29 +0200

    extracted some constants

--- src/mada_1/App.java
+++ src/mada_1/App.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class App {
 	private static final String CHIFFRE_TEXT_FILE_NAME = "/chiffre.txt";
+	private static final String PRIVATE_KEY_FILE_NAME = "/sk.txt";
+	private static final String PUBLIC_KEY_FILE_NAME = "/pk.txt";
+	private static final String TEXT_FILE_NAME = "/text.txt";
 	private FileUtils fileUtils = new FileUtils();
 
 	public static void main(String[] args) {
@@ -17,13 +20,13 @@ public class App {
 		
 
 		// encrypt text.txt in chiffre.txt
-		Tuple publicKey = getTupleFromFile("/pk.txt");
-		File textFile = fileUtils.getFileFromClassPath("/text.txt");
+		Tuple publicKey = getTupleFromFile(PUBLIC_KEY_FILE_NAME);
+		File textFile = fileUtils.getFileFromClassPath(TEXT_FILE_NAME);
 		String plainText = fileUtils.readContentFromFile(textFile);
 		String encrypted = encrypt(publicKey, plainText);
 
 		File chiffreFile = fileUtils.getFileFromClassPath(CHIFFRE_TEXT_FILE_NAME);
-		Tuple privateKey = getTupleFromFile("/sk.txt");
+		Tuple privateKey = getTupleFromFile(PRIVATE_KEY_FILE_NAME);
 		String chiffreText = fileUtils.readContentFromFile(chiffreFile);
 		String decrypted = decrypt(privateKey, chiffreText);
 		System.out.println(plainText + " - " + encrypted + " - " + decrypted);
